b56848c77a1d8647d2bc6941709490cd7da4a27c
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2025-05-14 17:29:12 +0100

    chore: add deploy script

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -213,11 +213,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:postgres@localhost:5432/afcfta?schema=public"
+        "value": null
       }
     }
   },
